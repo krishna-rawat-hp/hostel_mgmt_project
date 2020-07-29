dd7c7c92fe3881ff469cddf644b38baeb12d330d
krishna-rawat-hp <rawatkrishnakant8319@gmail.com>
2020-07-29 15:17:34 +0530

    common lib folder

---
+++ common_lib/navbar_first.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<link rel="stylesheet" type="text/css" href="../css/nav.css">
+
+</head>
+<body>
+	<div id="sidebar">
+		<ul>
+			<li><a href="../pages/index.php">Home</a></li>
+			<li><a href="../pages/reg.php">User Reg</a></li>
+			<li><a href="../pages/user_login.php">User Login</a></li>
+			<li><a href="../pages/admin_login.php">Admin Login</a></li>
+		</ul>
+	</div>
+</body>
+</html>
---
+++ common_lib/title.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOSTEL MANAGEMENT</title>
+</head>
+<body>
+
+</body>
+</html>
